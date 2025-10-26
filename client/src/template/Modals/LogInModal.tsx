93cc102bfd59210d817acc8c04317ac2ca5b29ad
unknown <dudgns113@gmail.com>
2025-10-27 00:56:41 +0900

    25.10.27 [Client] 모달에 Enter, ESC 키 추가
    :heavy_plus_sign: [Client_Modal] 로그인, 회원가입 모달에 Enter, ESC 키 추가
    :art: [Client_Modal] 로그인 모달 스타일 추가
    
    :construction: [Client] Header 기능
    - :construction: [Client] 회원 기능
        - :white_large_square: [Client] 회원가입 기능 추가
        - :white_large_square: [Client] 로그인 기능 추가

--- client/src/template/Modals/LogInModal.tsx
+++ client/src/template/Modals/LogInModal.tsx
@@ -1,37 +1,68 @@
-import {useCallback} from 'react'
+import {useCallback, useState} from 'react'
 
 import {Modal} from '@component'
 import {useModalActions} from '@redux'
 
-import type {FC} from 'react'
+import type {FC, FormEvent, KeyboardEvent} from 'react'
 import type {DivCommonProps} from '@prop'
 
 import './_styles/_ModalCommon.scss'
-import './_styles/SignUpModal.scss'
+import './_styles/LogInModal.scss'
 
 type LogInModalProps = DivCommonProps & {}
 
 export const LogInModal: FC<LogInModalProps> = ({className, ...props}) => {
   const {closeModal} = useModalActions()
+
+  const [userId, setUserId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const _executeLogIn = useCallback((userId: string, password: string) => {
+    console.log(userId, password)
+  }, [])
+
   const onClose = useCallback(() => {
     closeModal()
   }, [closeModal])
 
+  const onKeyDown = useCallback(
+    (userId: string, password: string) => (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        _executeLogIn(userId, password)
+      } // ::
+      else if (e.key === 'Escape') {
+        onClose()
+      }
+    },
+    [_executeLogIn, onClose]
+  )
+  const onSubmit = useCallback(
+    (userId: string, password: string) => (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      _executeLogIn(userId, password)
+    },
+    [_executeLogIn]
+  )
   return (
-    <Modal className={`LogIn_Modal _modal_template ${className || ''}`} onClose={onClose} {...props}>
+    <Modal className={`LogIn_Modal _modal_template ${className || ''}`} onClose={onClose} onKeyDown={onKeyDown(userId, password)} {...props}>
       {/* 1. 타이틀 */}
       <p className="_title_modal">로그인</p>
 
       {/* 2. 폼 */}
-      <form className="_form_modal">
+      <form className="_form_modal" onSubmit={onSubmit(userId, password)}>
+        {/* 2-1. ID */}
         <div className="_form_item">
           <label htmlFor="userId">ID</label>
-          <input autoFocus type="text" id="userId" name="userId" tabIndex={1} />
+          <input autoFocus type="text" id="userId" name="userId" required value={userId} onChange={e => setUserId(e.target.value)} tabIndex={1} />
         </div>
+
+        {/* 2-2. 비밀번호 */}
         <div className="_form_item">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" name="password" tabIndex={4} />
+          <input type="password" id="password" name="password" required value={password} onChange={e => setPassword(e.target.value)} tabIndex={4} />
         </div>
+
+        {/* 2-3. 버튼 */}
         <div className="_form_button_row">
           <button className="_button_form" type="submit" tabIndex={6}>
             로그인
--- client/src/template/Modals/SignUpModal.tsx
+++ client/src/template/Modals/SignUpModal.tsx
@@ -1,9 +1,9 @@
-import {useCallback} from 'react'
+import {useCallback, useState} from 'react'
 
 import {Modal} from '@component'
 import {useModalActions} from '@redux'
 
-import type {FC} from 'react'
+import type {FC, FormEvent, KeyboardEvent} from 'react'
 import type {DivCommonProps} from '@prop'
 
 import './_styles/_ModalCommon.scss'
@@ -13,37 +13,96 @@ type SignUpModalProps = DivCommonProps & {}
 
 export const SignUpModal: FC<SignUpModalProps> = ({className, ...props}) => {
   const {closeModal} = useModalActions()
+
+  const [userId, setUserId] = useState<string>('')
+  const [userMail, setUserMail] = useState<string>('')
+  const [userName, setUserName] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('')
+
+  const _executeSignUp = useCallback(
+    (userId: string, userMail: string, userName: string, password: string, passwordConfirm: string) => {
+      console.log(userId, userMail, userName, password, passwordConfirm)
+      closeModal()
+    },
+    [closeModal]
+  )
+
   const onClose = useCallback(() => {
     closeModal()
   }, [closeModal])
 
+  const onKeyDown = useCallback(
+    (userId: string, userMail: string, userName: string, password: string, passwordConfirm: string) => (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        _executeSignUp(userId, userMail, userName, password, passwordConfirm)
+      } // ::
+      else if (e.key === 'Escape') {
+        onClose()
+      }
+    },
+    [_executeSignUp, onClose]
+  )
+
+  const onSubmit = useCallback(
+    (userId: string, userMail: string, userName: string, password: string, passwordConfirm: string) => (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      _executeSignUp(userId, userMail, userName, password, passwordConfirm)
+    },
+    [_executeSignUp]
+  )
+
   return (
-    <Modal className={`SignUp_Modal _modal_template ${className || ''}`} onClose={onClose} {...props}>
+    <Modal
+      className={`SignUp_Modal _modal_template ${className || ''}`}
+      onClose={onClose}
+      onKeyDown={onKeyDown(userId, userMail, userName, password, passwordConfirm)}
+      {...props}
+    >
       {/* 1. 타이틀 */}
       <p className="_title_modal">회원가입</p>
 
       {/* 2. 폼 */}
-      <form className="_form_modal">
+      <form className="_form_modal" onSubmit={onSubmit(userId, userMail, userName, password, passwordConfirm)}>
+        {/* 2-1. ID */}
         <div className="_form_item">
           <label htmlFor="userId">ID</label>
-          <input autoFocus type="text" id="userId" name="userId" tabIndex={1} />
+          <input autoFocus type="text" id="userId" name="userId" required value={userId} onChange={e => setUserId(e.target.value)} tabIndex={1} />
         </div>
+
+        {/* 2-2. Email */}
         <div className="_form_item">
           <label htmlFor="userMail">Email</label>
-          <input type="email" id="userMail" name="userMail" tabIndex={2} />
+          <input type="email" id="userMail" name="userMail" required value={userMail} onChange={e => setUserMail(e.target.value)} tabIndex={2} />
         </div>
+
+        {/* 2-3. 이름 */}
         <div className="_form_item">
           <label htmlFor="userName">이름</label>
-          <input type="text" id="userName" name="userName" tabIndex={3} />
+          <input type="text" id="userName" name="userName" required value={userName} onChange={e => setUserName(e.target.value)} tabIndex={3} />
         </div>
+
+        {/* 2-4. 비밀번호 */}
         <div className="_form_item">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" name="password" tabIndex={4} />
+          <input type="password" id="password" name="password" required value={password} onChange={e => setPassword(e.target.value)} tabIndex={4} />
         </div>
+
+        {/* 2-5. 비밀번호 확인 */}
         <div className="_form_item">
           <label htmlFor="passwordConfirm">비밀번호 확인</label>
-          <input type="password" id="passwordConfirm" name="passwordConfirm" tabIndex={5} />
+          <input
+            type="password"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            required
+            value={passwordConfirm}
+            onChange={e => setPasswordConfirm(e.target.value)}
+            tabIndex={5}
+          />
         </div>
+
+        {/* 2-6. 버튼 */}
         <div className="_form_button_row">
           <button className="_button_form" type="submit" tabIndex={6}>
             가입
