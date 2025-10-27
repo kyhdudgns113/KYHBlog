ca0be2e5f61c46b819e1f3072f5fed4b9b0158b5
unknown <dudgns113@gmail.com>
2025-10-27 10:20:29 +0900

    25.10.27 [Client] Header 타이틀 추가 및 스타일 조정
    :heavy_plus_sign: [Client] Header 기능

--- client/src/template/Header/parts/TitleAreaPart.tsx
+++ client/src/template/Header/parts/TitleAreaPart.tsx
@@ -1,3 +1,6 @@
+import {useCallback} from 'react'
+import {useNavigate} from 'react-router-dom'
+
 import type {FC} from 'react'
 import type {DivCommonProps} from '@prop'
 
@@ -6,9 +9,17 @@ import '../_styles/Part_TitleArea.scss'
 type TitleAreaPartProps = DivCommonProps & {}
 
 export const TitleAreaPart: FC<TitleAreaPartProps> = ({className, ...props}) => {
+  const navigate = useNavigate()
+
+  const onClickTitle = useCallback(() => {
+    navigate('/')
+  }, [navigate]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className={`TitleArea_Part ${className || ''}`} {...props}>
-      TitleAreaPart
+      <p className="_title_part" onClick={onClickTitle}>
+        강영훈의 블로그
+      </p>
     </div>
   )
 }
