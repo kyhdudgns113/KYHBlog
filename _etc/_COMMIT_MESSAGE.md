52232d5548b4db74cc41b4d24d0a3073ca21d889
unknown <dudgns113@gmail.com>
2025-12-18 01:50:58 +0900

    25.12.18 [Client] QnA 페이지 글쓰기 버튼 기능 조정
    :wrench: [Client] QnA 글쓰기 버튼 로그인 안되어있으면 navigate 조차 되지 않게 수정
    
    TODO List
      :white_large_square: 홈 페이지

--- _etc/_COMMIT_MESSAGE.md
+++ _etc/_COMMIT_MESSAGE.md
@@ -1,6 +1,6 @@
+25.12.18 [Client] QnA 페이지 글쓰기 버튼 기능 조정
 
-:hammer: [Server] 관리자는 관리자에게 채팅하기 못 쓰도록 조정
-:hammer: [Client] 관리자는 관리자에게 채팅하기 못 쓰도록 조정
+:wrench: [Client] QnA 글쓰기 버튼 로그인 안되어있으면 navigate 조차 되지 않게 수정
 
 TODO List
   :white_large_square: 홈 페이지
--- client/src/pages/QnA/QnA_Page/buttons/WriteQnAButton/WriteQnAButton.tsx
+++ client/src/pages/QnA/QnA_Page/buttons/WriteQnAButton/WriteQnAButton.tsx
@@ -1,6 +1,9 @@
 import {useCallback} from 'react'
 import {useNavigate} from 'react-router-dom'
 
+import {useAuthStatesContext} from '@context'
+import {useTemplateActions} from '@redux'
+
 import type {FC, MouseEvent} from 'react'
 import type {DivCommonProps} from '@prop'
 
@@ -8,16 +11,36 @@ import './WriteQnAButton.scss'
 
 type WriteQnAButtonProps = DivCommonProps & {}
 
+/**
+ * WriteQnAButton
+ * - 글쓰기 버튼
+ * - QnA 페이지에 있음
+ * - QnA 작성 페이지로 이동하는 버튼
+ */
 export const WriteQnAButton: FC<WriteQnAButtonProps> = ({className, ...props}) => {
+  const {clickLogInBtn} = useTemplateActions()
+
+  const {isLoggedIn} = useAuthStatesContext()
+
   const navigate = useNavigate()
 
-  const onClickWrite = useCallback((e: MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation()
-    navigate('/main/qna/write')
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  const onClickWrite = useCallback(
+    (isLoggedIn: boolean) => (e: MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation()
+
+      if (!isLoggedIn) {
+        clickLogInBtn()
+        alert('로그인 이후 이용해주세요.')
+        return
+      }
+
+      navigate('/main/qna/write')
+    },
+    [] // eslint-disable-line react-hooks/exhaustive-deps
+  )
 
   return (
-    <div className={`WriteQnAButton ${className || ''}`} onClick={onClickWrite} {...props}>
+    <div className={`WriteQnAButton ${className || ''}`} onClick={onClickWrite(isLoggedIn)} {...props}>
       글쓰기
     </div>
   )
