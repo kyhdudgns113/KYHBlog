9e8ffcbf23b5ca48465aad65d90d4c46042f3f58
unknown <dudgns113@gmail.com>
2026-01-24 16:59:57 +0900

    26.01.24 :wrench: [Client] 홈 탭 환영문구 추가
    - :white_check_mark: [Client] 홈/ 최근 게시물
      - :wrench: [Client] 홈 탭 환영문구 추가
    
    - :white_large_square: [Client] 블로그/ 새 게시물
    - :white_large_square: [Server] 일부 DB 메모리화

--- client/src/pages/HomePage/parts/IntroPart/IntroPart.tsx
+++ client/src/pages/HomePage/parts/IntroPart/IntroPart.tsx
@@ -8,7 +8,7 @@ type IntroPartProps = DivCommonProps & {}
 export const IntroPart: FC<IntroPartProps> = ({...props}) => {
   return (
     <div className={`Intro_Part`} {...props}>
-      <p>블로그에 오신걸 환영합니다~</p>
+      <p className="_title_part">블로그에 오신걸 환영합니다~</p>
     </div>
   )
 }
